a0ca22743f62154317200ee62459c7a25749ea33
Joonas <joonsyys@gmail.com>
2019-02-21 12:21:02 +0200

    Add API endpoints for Team and Game lists

--- app/project/premier/views.py
+++ app/project/premier/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from premier.models import Team, Game
+from premier.serializers import TeamSerializer, GameSerializer
+from rest_framework import generics
 
 # Create your views here.
+class TeamListCreate(generics.ListCreateAPIView):
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer
+
+class GameListCreate(generics.ListCreateAPIView):
+    queryset = Game.objects.all()
+    serializer_class = GameSerializer
